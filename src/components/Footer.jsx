52926ffe6539610876ac057829ffb6e881a9aaa2
Lort974 <laurent.bigey@outlook.fr>
2024-03-17 09:32:54 +0100

    test

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 const Footer = () => {
-  return <footer>© 2024 Wealth Health - all rights reserved</footer>;
+  return <footer>© 2024 Wealth Health - all rights reserveddd</footer>;
 };
 
 export default Footer;
